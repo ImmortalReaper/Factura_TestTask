0a2b641bdd6bdb50aba6e963a42aea5cb5166ff5
ImmortalReaper <Artem172902@gmail.com>
2025-05-30 19:10:01 +0300

    Adress

---
+++ Assets/Scripts/AddressablesAddress/Address.cs
@@ -0,0 +1,39 @@
+namespace AddressablesAddress
+{
+    public static class Address
+    {
+        public static class Weather
+        {
+            public const string SandStorm = "SandStorm";
+        }
+
+        public static class UI
+        {
+            public const string WinUI = "WinUI";
+            public const string LoseUI = "LoseUI";
+        }
+
+        public static class Scenes
+        {
+            public const string Gameplay = "Gameplay";
+        }
+        
+        public static class Configs
+        {
+            public const string CarStatsConfig = "CarStatsConfig";
+            public const string BulletConfig = "BulletConfig";
+            public const string GameLevelsConfig = "GameLevelsConfig";
+            public const string TurretConfig = "TurretConfig";
+            public const string EnemyConfigs = "EnemyConfigs";
+            public const string WeatherConfigs = "WeatherConfigs";
+            public const string DesertRoadTypeConfig = "DesertRoadTypeConfig";
+        }
+    
+        public static class Prefabs
+        {
+            public const string DesertRoad = "DesertRoad";
+            public const string Stickman = "Stickman";
+            public const string Car = "Car";
+        }
+    }
+}
