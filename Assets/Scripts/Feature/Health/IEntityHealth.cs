903c95387d2f56136baa8ca36b3d1899ee128647
ImmortalReaper <Artem172902@gmail.com>
2025-05-30 19:14:15 +0300

    Entity Health

--- Assets/Scripts/Feature/Health/IEntityHealth.cs
+++ Assets/Scripts/Feature/Health/IEntityHealth.cs
@@ -1,8 +1,14 @@
+using System;
+
 public interface IEntityHealth
 {
     public float Health { get; }
     public float MaxHealth { get; }
+    public event Action<float> OnHealthChange;
+    public event Action OnDamageTaken;
+    public event Action<ContactData> OnContactDamageTaken;
+    public event Action OnDeath;
     public void SetMaxHealth(float maxHealth);
     public void SetHealth(float health);
-    public void TakeDamage(float damage);
+    public void TakeDamage(float damage, ContactData contactData = default);
 }
