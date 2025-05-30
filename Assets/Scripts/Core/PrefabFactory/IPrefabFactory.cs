e950ce0c5d1c3083ab410350bfe0f92cbded4b08
ImmortalReaper <Artem172902@gmail.com>
2025-05-30 19:11:19 +0300

    Prefab Factory

--- Assets/Scripts/Core/PrefabFactory/IPrefabFactory.cs
+++ Assets/Scripts/Core/PrefabFactory/IPrefabFactory.cs
@@ -4,6 +4,10 @@ namespace Core.PrefabFactory
 {
     public interface IPrefabFactory
     {
+        public GameObject Create(GameObject prefab);
+        public GameObject Create(GameObject prefab, Vector3 position, Transform parent = null);
+        public GameObject Create<T>(T prefab) where T : Component;
+        public GameObject Create<T>(T prefab, Vector3 position, Transform parent = null) where T : Component;
         public GameObject Create(string prefabName);
         public GameObject Create(string prefabName, Vector3 position, Transform parent = null);
     }
--- Assets/Scripts/Core/PrefabFactory/PrefabFactory.cs
+++ Assets/Scripts/Core/PrefabFactory/PrefabFactory.cs
@@ -14,13 +14,28 @@ namespace Core.PrefabFactory
             _addressablesAssetLoaderService = addressablesAssetLoaderService;
             _diContainer = container;
         }
-    
+
+        public GameObject Create(GameObject prefab)
+        {
+            return _diContainer.InstantiatePrefab(prefab);
+        }
+        public GameObject Create(GameObject prefab, Vector3 position, Transform parent = null)
+        {
+            return _diContainer.InstantiatePrefab(prefab);
+        }
+        public GameObject Create<T>(T prefab) where T : Component
+        {
+            return _diContainer.InstantiatePrefab(prefab);
+        }
+        public GameObject Create<T>(T prefab, Vector3 position, Transform parent = null) where T : Component
+        {
+            return _diContainer.InstantiatePrefab(prefab);
+        }
         public GameObject Create(string prefabName)
         {
             GameObject prefab = _addressablesAssetLoaderService.LoadAsset<GameObject>(prefabName);
             return _diContainer.InstantiatePrefab(prefab);
         }
-
         public GameObject Create(string prefabName, Vector3 position, Transform parent = null)
         {
             GameObject prefab = _addressablesAssetLoaderService.LoadAsset<GameObject>(prefabName);
